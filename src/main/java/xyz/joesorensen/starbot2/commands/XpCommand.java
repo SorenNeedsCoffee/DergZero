9eb9ebbc6c47b5ec318ab81bf67079c0fcefb8e9
Soren Dangaard <joseph.md.sorensen@gmail.com>
2019-09-25 15:31:01 -0600

    Revert "wot"
    This reverts commit 5c2242eb

---
+++ src/main/java/xyz/joesorensen/starbot2/commands/XpCommand.java
@@ -0,0 +1,19 @@
+package xyz.joesorensen.starbot2.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+
+/**
+ *   -=StarBot2=-
+ *  @author Soren Dangaard (joseph.md.sorensen@gmail.com)
+ *
+ */
+public abstract class XpCommand extends Command {
+
+    protected XpCommand() {
+
+        this.category = new Category("User");
+        this.guildOnly = true;
+
+    }
+
+}
