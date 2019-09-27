d60b8ef760165344d4da35647257c6d461d5af9f
Soren Dangaard <joseph.md.sorensen@gmail.com>
2019-09-26 19:41:13 -0600

    progress bar fix

--- src/main/java/xyz/joesorensen/starbot2/commands/xp/LvlCmd.java
+++ src/main/java/xyz/joesorensen/starbot2/commands/xp/LvlCmd.java
@@ -37,7 +37,7 @@ public class LvlCmd extends XpCommand {
         embed.addField("XP", new DecimalFormat("#.##").format(user.getXp()), false);
         embed.addField("Progress to next level",
                 "```java\n" +
-                        progress(user.getXp() / (user.getLvl() * 250)) +
+                        progress((user.getXp() - ((user.getLvl() * 250) - 250)) / ((user.getLvl() * 250) - 250)) +
                         " (" + new DecimalFormat("#.##").format(user.getXp()) + "/" + (user.getLvl() * 250) + ")" +
                         "\n```",
                 false);
